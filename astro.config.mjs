0a8f02012c53869732975b27c04de6561822828d
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-07-08 12:00:27 +0300

    add compressHTML: true for v7 compatibility and fix tidio script is:inline

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || 'http://localhost:4321',
   output: 'static',
   trailingSlash: 'never',
+  compressHTML: true,
   integrations: [
     mdx(),
     sitemap(),
